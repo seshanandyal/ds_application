880c3cdb463ab3c1dcc95a35c9ae69d8c934334e
Sesha Nandyal <sesha.nandyal@gmail.com>
2017-08-25 09:30:13 -0600

    moved MaxStack.java to com.veda.stacksnqueue package initial commit for MinStack.java

---
+++ src/com/veda/stacksnqueue/MinStack.java
@@ -0,0 +1,73 @@
+package com.veda.stacksnqueue;
+
+import java.util.Stack;
+
+public class MinStack {
+	Stack<Integer> mStack;
+	Stack<Integer> mMinStack;
+	
+	public MinStack() {
+		mStack = new Stack<>();
+		mMinStack = new Stack<>();
+	}
+	
+	public void push(int value) {
+		mStack.push(value);
+		
+		if(mMinStack.isEmpty() || mMinStack.peek() >= value) {
+			mMinStack.push(value);
+		}
+	}
+	
+	public int pop() {
+		int item = mStack.pop();
+		if(item == mMinStack.peek()) {
+			mMinStack.pop();
+		}
+		return item;
+	}
+	
+	public int min() throws StackEmptyException {
+		if(mMinStack.isEmpty()) {
+			throw new StackEmptyException("Stack is empty.");
+		}
+		
+		return mMinStack.peek();
+	}
+	
+	@Override
+	public String toString() {
+		return mStack.toString();
+	}
+	
+	public static void main(String[] args) throws StackEmptyException{
+		MinStack minStack
+		= new MinStack();
+		
+		minStack.push(5);
+		minStack.push(16);
+		minStack.push(3);
+		
+		System.out.println(minStack);
+		
+		System.out.println("Min: " + minStack.min());
+		
+		minStack.pop();
+		System.out.println(minStack);
+		
+		System.out.println("Min: " + minStack.min());
+	}
+	
+	class StackEmptyException extends Exception {
+		String mMessage;
+		
+		StackEmptyException() {
+			super();
+		}
+		
+		StackEmptyException(String message) {
+			super(message);
+			mMessage = message;
+		}
+	}
+}
