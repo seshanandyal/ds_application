dad5a1eeeba63a2f6eb5473a52dda350b92d3c10
Sesha Nandyal <sesha.nandyal@gmail.com>
2017-09-11 08:44:58 -0600

    Initial commit

---
+++ src/com/veda/stacksnqueue/BalancedBrackets.java
@@ -0,0 +1,53 @@
+package com.veda.stacksnqueue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancedBrackets {
+    static final char OPEN_PAREN = '(';
+    static final char CLOSE_PAREN = ')';
+    static final char OPEN_BRACES = '{';
+    static final char CLOSE_BRACES = '}';
+    static final char OPEN_SQUARE_BRACKET = '[';
+    static final char CLOSE_SQUARE_BRACKET = ']';
+    
+    public static boolean isBalanced(String expression) {
+        if("".equals(expression)) {
+            return true;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+        for(char ch: expression.toCharArray()) {
+            if(ch == OPEN_PAREN || ch == OPEN_BRACES || ch == OPEN_SQUARE_BRACKET) {
+                stack.push(ch);
+            } else {
+                if(ch == CLOSE_PAREN) {
+                    if(stack.isEmpty() || stack.pop() != OPEN_PAREN) {
+                        return false;
+                    }
+                } else if(ch == CLOSE_BRACES) {
+                    if(stack.isEmpty() || stack.pop() != OPEN_BRACES) {
+                        return false;
+                    }
+                } else if (ch == CLOSE_SQUARE_BRACKET) {
+                    if(stack.isEmpty() || stack.pop() != OPEN_SQUARE_BRACKET) {
+                        return false;
+                    }
+                }
+            }
+        }
+        if(stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+  
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            String expression = in.next();
+            System.out.println( (isBalanced(expression)) ? "YES" : "NO" );
+        }
+    }
+}
